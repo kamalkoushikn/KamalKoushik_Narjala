3ecc158d8df9af16c50375588d2dbd34a8b04f85
kamalkoushikn <kkousik63@gmail.com>
2023-12-05 00:06:52 -0800

    Created Contact List

--- Contacts/views.py
+++ Contacts/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from .models import Contact
+from .contact_form import ContactForm
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView
+)
 
 from .models import Contact
 
 # Create your views here.
 class ContactListView(ListView):
     model = Contact
+
+class ContactCreateView(CreateView):
+    model = Contact
+    form_class = ContactForm
+    success_url = reverse_lazy("contact_list")
