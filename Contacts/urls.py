f836ba287944535955ce152ff73486e67162d5ca
kamalkoushikn <kkousik63@gmail.com>
2023-12-05 15:39:57 -0800

    Edit & Delete Icons Updated

--- Contacts/urls.py
+++ Contacts/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from .views import (
-    ContactListView,
-    ContactCreateView
-    )
+from .views import ContactListView,ContactCreateView
+    
 
 urlpatterns = [
     path("", ContactListView.as_view(), name="contact_list"),
-    path("create/", ContactCreateView.as_view(), name="contact_create"),
+    path("create/", ContactCreateView, name="contact_form"),
 ]
--- Contacts/views.py
+++ Contacts/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Contact
 from .contact_form import ContactForm
 from django.urls import reverse_lazy
-from django.views.generic import (
-    ListView,
-    CreateView
-)
+from django.views.generic import ListView
 
 from .models import Contact
 
@@ -13,7 +10,14 @@ from .models import Contact
 class ContactListView(ListView):
     model = Contact
 
-class ContactCreateView(CreateView):
-    model = Contact
-    form_class = ContactForm
-    success_url = reverse_lazy("contact_list")
+
+def ContactCreateView(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  # This will create a new Contact instance
+            return redirect('contact_list')  # Redirect to the contact list page or any other page
+    else:
+        form = ContactForm()
+
+    return render(request, 'contacts/contact_form.html', {'form': form})
