ba16374b7596f6768e59b40759e99a08c661f568
kamalkoushikn <kkousik63@gmail.com>
2023-12-06 11:42:04 -0800

    Delete Contact Record Page

--- Contacts/views.py
+++ Contacts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from .models import Contact
 from .contact_form import ContactForm
 from django.urls import reverse_lazy
-from django.views.generic import ListView,DetailView,UpdateView
+from django.views.generic import View,ListView,DetailView,UpdateView,DeleteView
 
 from .models import Contact
 
@@ -11,16 +11,31 @@ class ContactListView(ListView):
     model = Contact
 
 
-def ContactCreateView(request):
-    if request.method == 'POST':
+class ContactCreateView(View):
+    template_name = 'contacts/contact_form.html'
+
+    def get(self, request):
+        form = ContactForm()
+        context = {
+            'form': form,
+            'is_creation': True  
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()  # This will create a new Contact instance
+            form.save()  
             return redirect('contact_list')  # Redirect to the contact list page or any other page
-    else:
-        form = ContactForm()
-
-    return render(request, 'contacts/contact_form.html', {'form': form})
+   
+        return render(request, self.template_name, {'form': form})
+    
+class ContactUpdateView(UpdateView):
+    model = Contact
+    form_class = ContactForm
+    template_name = 'contacts/contact_form.html'  # Use the same form template for update
+    success_url = reverse_lazy("contact_list")
 
 class ContactDetailView(DetailView):
     model = Contact
@@ -28,4 +43,15 @@ class ContactDetailView(DetailView):
 class ContactUpdateView(UpdateView):
     model = Contact
     form_class = ContactForm
+    success_url = reverse_lazy("contact_list")
+    template_name = 'contacts/contact_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Pass an additional variable to the template
+        context['is_creation'] = False  # Set it to True for update, False for create
+        return context
+    
+class ContactDeleteView(DeleteView):
+    model = Contact
     success_url = reverse_lazy("contact_list")
